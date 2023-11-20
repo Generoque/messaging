9e36efdc75b3674f3f950267cf7358624f94d1ae
Generoque <111399131+Generoque@users.noreply.github.com>
2023-11-20 22:08:01 +0800

    Added Supplementary

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MessageList from './components/MessageList';
 import { createImageMessage, createLocationMessage, createTextMessage} from './utils/MessageUtils';
 import React, {Component} from 'react';
@@ -17,15 +17,40 @@ class Messenger extends Component {
  ],
 }
 handlePressMessage = (message) => {
-  console.log('Message Pressed', message);
+  Alert.alert(
+    "Delete Message",
+    'Are you sure you want to delete this message?',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: "Delete",
+        onPress: () => this.deleteMessage(message),
+        style: 'destructive'
+      },
+    ]
+  );
 };
+  deleteMessage = (message) => {
+    this.setState((prevState) => ({
+      messages: prevState.messages.filter((msg) => msg !== message),
+    }));
+  };
 
 renderMessageList() {
-  const {messages} = this.state;
-
   return (
     <View style={styles.content}>
-      <MessageList messages ={messages} onPressMessage={this.handlePressMessage}/>
+      {this.state.messages.map((message, index) => (
+        <TouchableOpacity
+        key={index}
+        onPress={() => this.handlePressMessage(message)}>
+          <View style={styles.messageContainer}>
+            <MessageList messages={[message]} />
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -35,9 +60,24 @@ render () {
 
 }
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
   content: {
     flex:1,
+    width: '100%'
   },
+
+  messageContainer: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+
 });
 
 export default Messenger;
